ec94b2e21fe9110d0589469a33112c9c264bb540
EloáVM22 <127448572+EloaVasconcelos@users.noreply.github.com>
2023-04-17 08:31:52 -0300

    Add files via upload

---
+++ aula/script.js
@@ -0,0 +1,9 @@
+/*let msg = confirm ("Você é um aluno?")
+document.write ("Veja o resultado ", msg)
+
+let msg1 = prompt ("Digite seu nome:")
+document.write ("Valor digitado foi: ", msg1)*/
+
+function trocar (cor) {
+    document.body.style.background = cor;
+}
